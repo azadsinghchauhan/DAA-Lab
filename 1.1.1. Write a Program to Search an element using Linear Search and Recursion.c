8042da352759ce42c8001476db3bb9f5c2c26b2a
Azad Singh Chauhan <chauhanazad007@gmail.com>
2025-07-21 14:00:01 +0530

    Update 1.1.1. Write a Program to Search an element using Linear Search and Recursion.c

--- 1.1.1. Write a Program to Search an element using Linear Search and Recursion.c
+++ 1.1.1. Write a Program to Search an element using Linear Search and Recursion.c
@@ -1,3 +1,25 @@
+// Write a program to search the given element from a list of elements with linear search technique using recursion.
+
+
+
+// ﻿Note: Write the functions read1() and linearSearch() in Program911a.c
+
+
+// Sample Test Cases
+// Test case 1
+// Enter·n·value·:·4	
+// Enter·4·elements·:·10 20 15 12	
+// Enter·a·key·element·:·15	
+// The·key·element·15·is·found·at·position·:·2⏎	
+// Test case 2
+// Enter·n·value·:·6	
+// Enter·6·elements·:·2 6 4 1 3 7	
+// Enter·a·key·element·:·5	
+// The·key·element·5·is·not·found⏎
+
+
+
+
 #include <stdio.h>
 
 void read1(int a[], int n) {
