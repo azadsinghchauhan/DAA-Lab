3568a019047d4e52b2a49a81fefee410d01f2a07
Azad Singh Chauhan <chauhanazad007@gmail.com>
2025-10-13 13:34:37 +0530

    Update 1.1.12. N Queens Problem.c

--- 1.1.12. N Queens Problem.c
+++ 1.1.12. N Queens Problem.c
@@ -1,3 +1,34 @@
+
+
+/*Sample test case 1:
+
+Input:
+4 // First line of input is the size of the Chess board (square grid
+Output:
+2 // No of ways possible to place queens in safe state
+
+Sample test case 1:
+Input:
+2 // First line of input is the size of the Chess board (square grid
+Output:
+0 // No of ways possible to place queens in safe state
+
+You just have to complete the function nQueen() with parameter passed n represents the n x n size of the chess board and function returns the total no of solutions feasible to place queens in safe state, and if the their solution is no solution exists then return 0. 
+
+Constraints to be followed:
+2 ≤ n ≤ 10
+
+Instructions: To run your custom test cases strictly follow the input and output layout as mentioned in the visible sample test cases.
+
+Sample Test Cases
+Test case 1
+2	
+0	
+Test case 2
+3	
+0*/
+
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -33,4 +64,17 @@ int nQueen(int n) {
 	int board[MAX];
 	solveNQueen(0, n, board);
 	return count;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	if (n < 2 || n > 10) {
+		printf("0\n");
+	} else {
+		printf("%d\n", nQueen(n));
+	}
+
+	return 0;
 }
